cee2a89718f5f5632d60468c15e001244be8922b
Nilesoj20 <jvrn2014@gmail.com>
2023-08-14 22:16:32 -0700

    fix: Flask application listening on IP address 0.0.0.0.0 and port 5000 and displaying a message on the root route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,17 @@
+#!/usr/bin/python3
+""" script to start a Flask web application"""
 from flask import Flask
-app = flask(__name__)
 
-@app.route('/')
+
+app = Flask(__name__)
+
+
+@app.route("/")
 def index():
-    return '¡Hola HBNB!'
+    """fast route displaying a message"""
+    return "¡Hola HBNB!"
+
+
+if __name__ == '__main__':
+    """application listen on IP address 0.0.0.0.0 and port 5000"""
+    app.run(host='0.0.0.0', port=5000)
