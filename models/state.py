9dbd29f2cea4df24819535177fb09e8430c3c1de
Nilesoj20 <jvrn2014@gmail.com>
2023-08-16 16:20:08 -0500

    fix: add City method to return the list of City storage objects linked to the State stream

--- models/state.py
+++ models/state.py
@@ -16,18 +16,16 @@ class State(BaseModel, Base):
         cities = relationship("City", backref="state",
                               cascade="all, delete, delete-orphan")
     else:
-        name = ""
+        class State(BaseModel):
+            name = ""
 
-        def __init__(self, *args, **kwargs):
-            """initializes state"""
-            super().__init__(*args, **kwargs)
-
-        @property
-        def cities(self):
-            """return a list of city instances with state_id = current"""
-            all_instances = models.storage.all(City)
-            query = []
-            for key, value in all_instances.items():
-                if getattr(value, 'state_id') == self.id:
-                    query.append(value)
-            return query
+            @property
+            def cities(self):
+                """return a list of city instances with state_id = current"""
+                
+                all_instances = models.storage.all(City).values()
+                query = []
+                for ciudad in all_instances:
+                    if ciudad.state_id == self.id:
+                        query.append(ciudad)
+                return query
