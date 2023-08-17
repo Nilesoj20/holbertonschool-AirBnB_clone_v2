06a4c68b62770a41ec08a06272bd3705dff0d1c0
Nilesoj <118998391+Nilesoj20@users.noreply.github.com>
2023-08-17 09:03:20 -0500

    Fix: correcting the verification of the type of storage engine to be used

--- models/state.py
+++ models/state.py
@@ -8,23 +8,24 @@ import os
 import models
 
 
-class State(BaseModel, Base):
-    """ State class """
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    class State(BaseModel, Base):
+        """ State class """
+    
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete, delete-orphan")
-    else:
-        class State(BaseModel):
-            name = ""
+else:
+    class State(BaseModel):
+        name = ""
 
-            @property
-            def cities(self):
-                """return a list of city instances with state_id = current"""
-                all_instances = models.storage.all(City).values()
-                query = []
-                for ciudad in all_instances:
-                    if ciudad.state_id == self.id:
-                        query.append(ciudad)
-                return query
+        @property
+        def cities(self):
+            """return a list of city instances with state_id = current"""
+            all_instances = models.storage.all(City).values()
+            query = []
+            for ciudad in all_instances:
+                if ciudad.state_id == self.id:
+                    query.append(ciudad)
+            return query
