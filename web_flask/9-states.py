74b1f73e97644ceed8675b8e44c18e83d58abc37
Nilesoj20 <jvrn2014@gmail.com>
2023-08-19 12:02:49 -0500

    feat: showing cities and states on different routes

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Flask web application that lists the city """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def eliminar_session(self):
+    """delete the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def estados_lista():
+    """ path to print the list of states """
+    estados = storage.all(State)
+    return render_template('9-states.html', estados=estados)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def ciudades_lista(id):
+    """ path to print the list of cities """
+    estados = storage.all(State).values()
+    for x in estados:
+        if x.id == id:
+            return render_template('9-state.html', estados=estados, )
+    return render_template('9-states.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
