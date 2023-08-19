a3f59b25921dd827c997005f10b4a07c1c166a3c
Nilesoj20 <jvrn2014@gmail.com>
2023-08-19 15:18:08 -0500

    feat: applying dynamism to our website

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Flask web application that lists the city """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def eliminar_session(self):
+    """delete the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """ path to print the list of states """
+    estados = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           estados=estados, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
